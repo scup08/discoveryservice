73538c526c1c6c27756b562c01d8a196d9f24d29
scup08 <519370424@qq.com>
2017-06-28 16:10:26 +0800

    初始化提交

---
+++ src/main/java/com/xhld/discoveryservice/controller/TestController.java
@@ -0,0 +1,18 @@
+package com.xhld.discoveryservice.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+   @Value("${words}") String words;
+
+    @RequestMapping("/aaa")
+    public  String getWord() {
+        String[] wordArray = words.split(",");
+        int i = (int)Math.round(Math.random() * (wordArray.length - 1));
+        return wordArray[i];
+    }
+}
